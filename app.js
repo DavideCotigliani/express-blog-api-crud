1d495a70a4ae3e1d38c75a22c0a0c145cd81f37a
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-22 14:27:38 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app= express();
+const port= 4000;
+//asset statici
+app.use(express.static('public'));
+
+app.get("/",(req,res)=>{
+    res.send('ciao per i post');
+})
+
+//importo il router
+const portRouter = require('./routers/posts');
+
+// funzione use indico a express che ci sono nuove rotte specificando prefisso e router
+app.use("/posts", portRouter)
+
+app.listen(port,()=>{
+    console.log(`app in ascolto alla porta ${port}`);
+})
---
+++ routers/posts.js
@@ -0,0 +1,39 @@
+//importo express
+const express= require('express');
+//importo gli array
+const arrayPosts = require('../data/arraydipost')
+arrayPosts
+
+//creo variabile router il cui valore sarà un'istanza di express.router()
+const router = express.Router();
+
+//rotta per operazione index
+router.get('/', function(req,res){
+    res.json(arrayPosts)
+});
+
+//rotta per operazione show
+router.get('/:id', function (req,res){
+    res.send(`Visualizzazione singolo post: ${req.params.id}`)
+});
+
+//rotta per operazione store
+router.post('/', function (req,res){
+    res.send('Creazione nuovo post')
+});
+
+//rotta per operazione update
+router.put('/:id', function(req,res){
+    res.send(`Modifica integrale del post: ${req.params.id}`)
+})
+//rotta per operazione modify
+router.patch('/:id', function(req,res){
+    res.send(`Modifica parziale del post:${req.params.id}`)
+})
+//rotta per operazione destroy
+router.delete('/:id', function(req,res){
+    res.send(`Eliminazione del post: ${req.params.id}`)
+})
+
+//esporto l'istanza di router
+module.exports = router;
